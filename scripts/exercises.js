f3ae8281266d777d4b88f337fda5f8e667cea8c9
Justin Anderson <justinandersun@gmail.com>
2022-10-24 22:27:48 -0700

    Update exercise list and bug report

--- scripts/exercises.js
+++ scripts/exercises.js
@@ -1,39 +1,44 @@
 var pushExercises = [
-  ["Pushups",2,5,5,10,10,15,"url","description"],
-  ["Diamond Pushups",2,5,5,10,10,15,"url","description"],
-  ["Side-to-Side Pushups",2,5,5,10,10,15,"url","description"],
-  ["Tricep Dips",2,5,5,10,10,15,"url","description"],
-  ["Some Push",2,5,5,10,10,15,"url","description"]
+  ["Torso-Elevated Pushups",0,0,0,0,5,10,"Advanced","na","na"],
+  ["Hindu Pushups",0,0,0,0,5,10,"Advanced","na","na"],
+  ["Pushups",3,8,8,12,12,20,"Beginner","na","na"],
+  ["Tricep Dips",5,10,10,15,15,25,"Beginner","na","na"],
+  ["Diamond Pushups",0,0,5,10,8,15,"Intermediate","na","na"],
+  ["Wide Pushups",0,0,5,10,8,15,"Intermediate","na","na"],
+  ["Archer Pushups",0,0,6,10,8,15,"Intermediate","na","na"]
 ];
 
 var pullExercises = [
-  ["Pullups",2,3,3,8,5,12,"url","description"],
-  ["Inverted Rows",2,5,5,10,10,15,"url","description"],
-  ["Bar Hangs",2,5,5,10,10,15,"url","description"],
-  ["Chinups",2,5,5,10,10,15,"url","description"],
-  ["Some Pull",2,5,5,10,10,15,"url","description"]
+  ["Side-to-Side Pullups",0,0,0,0,5,10,"Advanced","na","na"],
+  ["Wide-Grip Pullups",0,0,0,0,5,10,"Advanced","na","na"],
+  ["Pullups",0,0,3,8,6,12,"Intermediate","na","na"],
+  ["Chinups",0,0,3,8,6,12,"Intermediate","na","na"]
 ];
 
 var coreExercises = [
-  ["Situps",2,5,5,10,10,15,"url","description"],
-  ["Russian Twists",2,5,5,10,10,15,"url","description"],
-  ["Bicycles",2,5,5,10,10,15,"url","description"],
-  ["Leg Raises",2,5,5,10,10,15,"url","description"],
-  ["Some Core",2,5,5,10,10,15,"url","description"]
+  ["Russian Twists",8,12,10,15,12,20,"Beginner","na","na"],
+  ["Bicycles",8,12,10,15,12,20,"Beginner","na","na"],
+  ["Crunches",10,15,12,20,15,25,"Beginner","na","na"],
+  ["Dead Bugs",5,8,8,12,12,15,"Beginner","na","na"],
+  ["Lying Leg Raises",8,12,10,15,12,20,"Beginner","na","na"],
+  ["Scissors",8,12,10,15,12,20,"Beginner","na","na"],
+  ["Bird Dogs",5,8,8,12,12,15,"Beginner","na","na"],
+  ["Situps",8,12,10,15,12,18,"Beginner","na","na"]
 ];
 
 var legExercises = [
-  ["Squats",2,5,5,10,10,15,"url","description"],
-  ["Calf Raises",2,5,5,10,10,15,"url","description"],
-  ["Pistol Squats",2,5,5,10,10,15,"url","description"],
-  ["One-Legged Squats",2,5,5,10,10,15,"url","description"],
-  ["Some Leg",2,5,5,10,10,15,"url","description"]
+  ["Pistol Squats",0,0,0,0,5,8,"Advanced","na","na"],
+  ["Squats",8,12,10,15,12,20,"Beginner","na","na"],
+  ["Lunges",5,10,8,12,10,15,"Beginner","na","na"],
+  ["Glute Bridges",8,12,10,15,12,20,"Beginner","na","na"],
+  ["Calf Raises",8,12,10,15,12,20,"Beginner","na","na"],
+  ["Side Lunges",5,10,8,12,10,15,"Beginner","na","na"],
+  ["Fire Hydrants",5,10,8,12,10,15,"Beginner","na","na"],
+  ["Single-Leg Deadlifts",0,0,5,10,8,12,"Intermediate","na","na"],
+  ["Sumo Squats",0,0,8,12,10,15,"Intermediate","na","na"]
 ];
 
 var fullExercises = [
-  ["Burpees",2,5,5,10,10,15,"url","description"],
-  ["Inchworms",2,5,5,10,10,15,"url","description"],
-  ["Prone Walkouts",2,5,5,10,10,15,"url","description"],
-  ["Dragon Flags",2,5,5,10,10,15,"url","description"],
-  ["Some Full",2,5,5,10,10,15,"url","description"]
+  ["Inchworms",0,0,5,8,8,12,"Intermediate","na","na"],
+  ["Burpees",0,0,5,8,8,12,"Intermediate","na","na"]
 ];
